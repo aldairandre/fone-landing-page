4fe7b50aa798ae443d3783ac976d522599b31fdf
Aldair André <aldairandre99@gmail.com>
2022-05-01 14:26:48 +0100

    versao para tabletes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,15 +4,15 @@ export default function Footer(){
             <div className="container-footer">
                 <div className="info-footer">
                     <i className="bi bi-activity"></i>
-                    <p>High-Resolution Audio <br/>compatible</p>
+                    <p>High-Resolution Audio compatible</p>
                 </div>
                 <div className="info-footer">
                     <i className="bi bi-bluetooth"></i>
-                    <p>Wireless connectivity using <br/>bluetooth</p>
+                    <p>Wireless connectivity using bluetooth</p>
                 </div>
                 <div className="info-footer">
                     <i className="bi bi-battery"></i>
-                    <p>A powerful battery that lasts <br/>up to 4 hours of use</p>
+                    <p>A powerful battery that lasts up to 4 hours of use</p>
                 </div>
             </div>
         </footer>
